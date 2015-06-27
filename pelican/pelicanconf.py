bde095b639881601e3b1e1dcbde5e1b3432390e9
Charles Reid <charlesreid1@gmail.com>
2015-06-27 14:36:21 -0700

    updating project page to have some angular javascript scaffolding. adding empty file to content folder/.

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -46,6 +46,9 @@ TEMPLATE_PAGES['index.html'] = 'index.html'
 
 # projects
 TEMPLATE_PAGES['projects.html'] = 'projects/index.html'
+TEMPLATE_PAGES['projects.json'] = 'projects/projects.json'
+TEMPLATE_PAGES['projects_modcontrol.js'] = 'projects/projects_modcontrol.js'
+TEMPLATE_PAGES['projects_sunburst.js'] = 'projects/projects_sunburst.js'
 
 # about
 TEMPLATE_PAGES['about.html'] = 'about/index.html'
