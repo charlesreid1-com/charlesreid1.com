f588832a9c259d755467963674e4235174fa7164
Charles Reid <charlesreid1@gmail.com>
2015-06-28 13:53:04 -0700

    adding fixes to consulting pages, inter-page references, variables

--- pelican/consulting/consulting_modcontrol.js
+++ pelican/consulting/consulting_modcontrol.js
@@ -1,4 +1,4 @@
-var a = angular.module("aboutApp", [], function($interpolateProvider) {
+var a = angular.module("consultingApp", [], function($interpolateProvider) {
             $interpolateProvider.startSymbol('[[');
             $interpolateProvider.endSymbol(']]');
         }
@@ -8,7 +8,7 @@ var datafactory = a.factory('datafactory', function($http, $q) {
     return {
         getNestData : function() {
             var deferred = $q.defer();
-            var filename = 'about.json';
+            var filename = 'consulting.json';
             $http.get(filename).success(function(data) {
                 deferred.resolve(data);
             }).error(function(){
@@ -24,7 +24,7 @@ function PController($scope,datafactory) {
     $scope.initialize = function() {
         datafactory.getNestData().then(
             function(data) { 
-                $scope.aboutData = data;
+                $scope.consultingData = data;
                 console.log(data);
             }
         );
@@ -32,5 +32,5 @@ function PController($scope,datafactory) {
 }
 
 // the first few arguments of the list should correspond to the Angular-namespace stuff to feed to HelloController
-var c = a.controller("aboutController", ["$scope", "datafactory", PController]);
+var c = a.controller("consultingController", ["$scope", "datafactory", PController]);
 
